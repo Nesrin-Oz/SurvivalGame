b15e93aa4825486e5d37f9314933797f12b35a22
Nesrin-Oz <02nesrinozdemir@gmail.com>
2026-08-17 16:58:43 +0300

    Gece/gündüz döngüsü entegre edildi ve kamera açı/çakışma sorunları giderildi
    Directional Light rotasyonu ile zaman bazlı çalışan DayNightCycle sistemi entegre edildi.
    
    Kameranın zemin altına girmesini engellemek için dikey bakış açısı Mathf.Clamp fonksiyonu ile sınırlandırıldı.

--- Assets/Script/DayNightCycle.cs
+++ Assets/Script/DayNightCycle.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 public class DayNightCycle : MonoBehaviour
 {
     [Header("Zaman Ayarları")]
-    public float dayDurationInSeconds = 120f; 
+    public float dayDurationInSeconds = 30f; 
 
     [Header("Güneş (Directional Light)")]
     public Transform directionalLight;
 
+    [Header("Gökyüzü Renk Geçişi (Tint)")]
+    public Gradient gokyuzuRenkleri; 
+
     private float timeMultiplier;
+    private Material dinamikSkybox;
 
     void Start()
     {
         timeMultiplier = 360f / dayDurationInSeconds;
+        
+        dinamikSkybox = new Material(RenderSettings.skybox);
+        RenderSettings.skybox = dinamikSkybox;
     }
 
     void Update()
@@ -20,6 +27,12 @@ public class DayNightCycle : MonoBehaviour
         if (directionalLight != null)
         {
             directionalLight.Rotate(Vector3.right * timeMultiplier * Time.deltaTime);
+
+            float gununYuzdesi = directionalLight.eulerAngles.x / 360f;
+
+           Color anlikRenk = gokyuzuRenkleri.Evaluate(gununYuzdesi);
+
+            dinamikSkybox.SetColor("_Tint", anlikRenk);
         }
     }
 }
